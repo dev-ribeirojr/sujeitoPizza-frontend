d1519b699d2b8e4770ff127ca0060ddac339ef3a
ribeiro-jr <ribeirojr1522@gmail.com>
2023-10-19 13:51:17 -0300

    chore: Adicionando icones no setup

---
+++ src/config/icons/index.tsx
@@ -0,0 +1,13 @@
+import { FaEye, FaEyeSlash, FaEnvelope, FaUser, FaUserPlus } from "react-icons/fa";
+import { MdLogout } from 'react-icons/md';
+import { PiSignInBold } from 'react-icons/pi';
+
+export const icons = {
+  email: <FaEnvelope />,
+  user: <FaUser />,
+  password: <FaEyeSlash />,
+  passwordVisible: <FaEye />,
+  newUser: <FaUserPlus />,
+  logOut: <MdLogout />,
+  signIn: <PiSignInBold />
+}
